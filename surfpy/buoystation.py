804c0fb27bd5576a66a80526c37bdb40efe1b4a2
Andrew Kallinikos <klinks22@gmail.com>
2026-09-16 21:13:40 -0400

    Add timeouts to fetches

--- surfpy/buoystation.py
+++ surfpy/buoystation.py
@@ -393,26 +393,28 @@ class BuoyStation(BaseStation):
 
     def fetch_latest_reading(self):
         print(self.latest_reading_url)
-        response = requests.get(self.latest_reading_url)
+        response = requests.get(self.latest_reading_url, timeout=30)
         if len(response.text) < 1:
             return None
         return self.parse_latest_reading_data(response.text)
 
     def fetch_meteorological_reading(self, data_count=20):
-        response = requests.get(self.meteorological_reading_url)
+        response = requests.get(self.meteorological_reading_url, timeout=30)
         if len(response.text) < 1:
             return None
         return self.parse_meteorological_reading_data(response.text, data_count)
 
     def fetch_detailed_wave_reading(self, data_count=20):
-        response = requests.get(self.detailed_wave_reading_url)
+        response = requests.get(self.detailed_wave_reading_url, timeout=30)
         if len(response.text) < 1:
             return None
         return self.parse_detailed_wave_reading_data(response.text, data_count)
 
     def fetch_wave_spectra_reading(self, data_count=20):
-        energy_response = requests.get(self.wave_energy_reading_url)
-        directional_response = requests.get(self.directional_wave_reading_url)
+        energy_response = requests.get(
+            self.wave_energy_reading_url, timeout=30)
+        directional_response = requests.get(
+            self.directional_wave_reading_url, timeout=30)
         if len(energy_response.text) < 1 or len(directional_response.text) < 1:
             return None
 
@@ -428,7 +430,7 @@ class BuoyStation(BaseStation):
     def fetch_wave_forecast_bulletin(self, model):
         url = self.wave_forecast_bulletin_url(model)
         print(url)
-        response = requests.get(url)
+        response = requests.get(url, timeout=30)
         if len(response.text) < 1:
             return None
         return self.parse_wave_forecast_bulletin(response.text, None)
